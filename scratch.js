1b69f347ef79dd27ed5ceb541d384dd2ff3bbe0c
dcarrsf <dan@dancarr.co>
2017-03-26 14:52:20 -0700

    Removing extra files.

--- scratch.js
+++
@@ -1,6 +0,0 @@
-
-        <IndexRoute component={Home}/>
-        <Route path="/code" component={Code}/>
-        <Route path="/projects" component={Projects}/>
-        <Route path="/publications" component={Publications}/>
-        <Route path="/story" component={Story}/>
