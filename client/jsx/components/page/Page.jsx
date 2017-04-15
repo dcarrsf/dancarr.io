64b3fa9ace556557f90ea4082577ef36fe9e1468
dcarrsf <dan@dancarr.co>
2017-04-14 20:26:02 -0700

    Updating Page to a stateless functional component.

--- client/jsx/components/page/Page.jsx
+++ client/jsx/components/page/Page.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import Details from '../details/Details';
 
-class Page extends React.Component {
-
-  // Draw the view...
-  render() {
-    // Calculations...
-    return(
-        <div className='row'>
-            <div className='col-xs-12 col-lg-4'>
-            <h2>{this.props.title}</h2>
-            </div>
-            <div className='col-xs-12 col-lg-8'>
-            <Details details={this.props.details} />
-            </div>
-        </div>
-    )
-  }
+const Page = (props) => {
+  return (
+    <div className='row'>
+      <div className='col-xs-12 col-lg-4'>
+        <h2>{props.title}</h2>
+      </div>
+      <div className='col-xs-12 col-lg-8'>
+        <Details details={props.details} />
+      </div>
+    </div>
+  );
 }
 
 export default Page;
