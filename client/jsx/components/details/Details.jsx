211d963d006fda912c2f79b21bd955ec0a98816a
dcarrsf <dan@dancarr.co>
2017-04-14 20:51:55 -0700

    Updating Detail to a stateless functional component.

--- client/jsx/components/details/Details.jsx
+++ client/jsx/components/details/Details.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import DetailItem from '../details/DetailItem.jsx';
 
-class Details extends React.Component {
-  // Draw
-  render() {
-    // Calculations...
-    const detailItems = this.props.details.map((detail, i) => {
-      const uniqid = `detail-${i}`;
-      return <DetailItem key={uniqid} {...detail} />;
-    });
-    return (
-      <article className='details'>{detailItems}</article>
-    );
-  }
+const Details = ({details}) => {
+  const detailItems = details.map((detail, i) => {
+    const uniqid = `detail-${i}`;
+    return <DetailItem key={uniqid} {...detail} />;
+  });
+  return (
+    <ul className='details'>
+      {detailItems}
+    </ul>
+  );
 }
 
 // Validate props
