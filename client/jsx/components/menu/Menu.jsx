89ec9ed9a512c1d9bf98c32d21a612fe5193a1b6
dcarrsf <dan@dancarr.co>
2017-04-14 21:37:50 -0700

    Updating Detail components and responsive page layout.

--- client/jsx/components/menu/Menu.jsx
+++ client/jsx/components/menu/Menu.jsx
@@ -6,12 +6,14 @@ const Menu = ({items, width, selectedIndex}) => {
   const len = items.length;
   let menuItems = [];
   items.forEach((item, i) => {
-    const isSelected = selectedIndex === i;
-    const uniqueKey = `item-${i}`;
-    menuItems.push(
-      <MenuItem key={uniqueKey} label={item.label} labelShort={item.labelShort}
-                icon={item.icon} width={width} route={item.route} selected={isSelected}/>
-    );
+    if (i > 0) {
+      const isSelected = selectedIndex === i;
+      const uniqueKey = `item-${i}`;
+      menuItems.push(
+        <MenuItem key={uniqueKey} label={item.label} labelShort={item.labelShort}
+                  icon={item.icon} width={width} route={item.route} selected={isSelected}/>
+      );
+    }
   });
   return (
     <nav>
