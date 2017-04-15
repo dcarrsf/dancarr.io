6e4ad510f6b197aa31dce60e8eb2ebb0d67f7d87
dcarrsf <dan@dancarr.co>
2017-04-14 20:47:05 -0700

    Updating DetailItem to a stateless functional component.

--- client/jsx/components/details/DetailItem.jsx
+++ client/jsx/components/details/DetailItem.jsx
@@ -1,29 +1,20 @@
 import React from 'react';
 
-class DetailItem extends React.Component {
-  // Lifecycle methods...
-  constructor(props) {
-    super(props);
-    this.handleClick = this.handleClick.bind(this);
+const DetailItem = ({title, description, url, icon, note, years, pub, type}) => {
+  const cls = !icon ? '' : `glyphicon glyphicon-${icon}`;
+  const handleClick = (e) => {
+    window.open(url, '_blank');
   }
-  // Open link
-  handleClick(event) {
-    window.open(this.props.url, '_blank');
-  }
-  render() {
-    // Calculations...
-    const cls = !this.props.icon ? '' : `glyphicon glyphicon-${this.props.icon}`;
-    return (
-        <li className='detail-item' onClick={this.handleClick}>
-            <h3>{this.props.title}</h3>
-            <p className='description'>{this.props.description}</p>
-            <p className='note'>{this.props.note}</p>
-            <p className='year'>{this.props.years}</p>
-            <p className='type'>{`${this.props.pub} ${this.props.type}`} <span className={cls}></span></p>
-            <div className="line"></div>
-        </li>
-    );
-  }
+  return (
+    <li className='detail-item' onClick={handleClick}>
+      <h3>{title}</h3>
+      <p className='description'>{description}</p>
+      <p className='note'>{note}</p>
+      <p className='year'>{years}</p>
+      <p className='type'>{`${pub} ${type}`} <span className={cls}></span></p>
+      <div className="line"></div>
+    </li>
+  );
 }
 
 // Validate props
