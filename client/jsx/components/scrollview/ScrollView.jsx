3887e035fa3011669b2f2c4e4148f04acb9be335
dcarrsf <dan@dancarr.co>
2017-04-14 19:58:48 -0700

    Adding debounce to scroll event timing.

--- client/jsx/components/scrollview/ScrollView.jsx
+++ client/jsx/components/scrollview/ScrollView.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import debounce from '../../utils/debounce';
 
 const ScrollView = ({width, height, scrollOffset, onScrollChange, children}) => {
   // Dynamically control height
@@ -13,7 +14,7 @@ const ScrollView = ({width, height, scrollOffset, onScrollChange, children}) => 
   }
   // Handle scroll change
   const scrollIt = (event) => {
-    onScrollChange(event.currentTarget.scrollTop);
+    debounce(onScrollChange(event.currentTarget.scrollTop, 100));
   };
   return (
     <section className='scroll-view' style={css} onScroll={scrollIt}>
@@ -23,48 +24,4 @@ const ScrollView = ({width, height, scrollOffset, onScrollChange, children}) => 
   );
 };
 
-/*class ScrollView extends React.Component {
-  // Draw
-  render() {
-    // Dynamically control height
-    const bottom = this.props.width >= 1024 ? 76 : 118;
-    const css = {
-      height: `${this.props.height - bottom}px`,
-    };
-    // Reset scroll position
-    if (this.props.scrollOffset === 0) {
-      const sv = document.getElementsByClassName('scroll-view')[0];
-      if (sv) {
-        sv.scrollTop = 0;
-      }
-    }
-    // Handle scroll change
-    const scrollIt = (event) => {
-      this.props.onScrollChange(event.currentTarget.scrollTop);
-    };
-    return ( 
-        <section className='scroll-view' style={css} onScroll={scrollIt}>
-          {this.props.children}
-        </section>
-    );
-  }
-}*/
-
 export default ScrollView;
-
-// Helper 
-
-// function throttle(func, wait) {
-//   var blocked = false;
-//   var cache = null;
-//   return function() {
-//     if (!blocked) {
-//       blocked = true;
-//       cache = func.call();
-//     }else{
-//       setTimeout(function() {
-//         blocked = false;
-//       }, wait);
-//     }
-//   }
-// }
