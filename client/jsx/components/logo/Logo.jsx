21a355a571cf966266a44abee47161b66432718d
dcarrsf <dan@dancarr.co>
2017-04-14 20:36:03 -0700

    Updating Logo to a stateless functional component.

--- client/jsx/components/logo/Logo.jsx
+++ client/jsx/components/logo/Logo.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-class Logo extends React.Component {
-  // Lifecycle methods...
-  render() {
-    // Calculations...
-    const cls = this.props.selected ? 'active' : 'normal';
-    return (
-      <div className='logo'>
-        <Link className={cls} to={this.props.route}>
-          <h1>{this.props.label}</h1>
-        </Link>
-      </div>
-    );
-  }
+const Logo = ({label, route, selected}) => {
+  const cls = selected ? 'active' : 'normal';
+  return (
+    <div className='logo'>
+      <Link className={cls} to={route}>
+        <h1>{label}</h1>
+      </Link>
+    </div>
+  );
 }
 
 // Validate props
