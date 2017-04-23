125f2329baa120115e9713060a885c2f0dc0b035
dcarrsf <dan@dancarr.co>
2017-04-23 10:19:09 -0700

    Clean up jsx and sass.

--- client/jsx/components/page/Page.jsx
+++ client/jsx/components/page/Page.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Details from '../details/Details';
 
-const Page = (props) => {
+const Page = ({title, details}) => {
   return (
     <div className='row'>
       <div className='col-xs-12 col-lg-4'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
       </div>
       <div className='col-xs-12 col-lg-8'>
-        <Details details={props.details} />
+        <Details details={details} />
       </div>
     </div>
   );
